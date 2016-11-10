41329d4e04202a939e1ade4a05ceb7c9ebe4b4b6
Technojackets <chesterpig96@gmail.com>
2016-11-10 14:51:33 -0500

    Quck update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 import static org.firstinspires.ftc.teamcode.TechnoJacketsHardware.fullStop;
 
